8629e45f7ad5cd59c0e277963ea89dab1676b545
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2017-10-28 18:46:32 +0900

    change tag list display

--- src/Model/Entity/Article.php
+++ src/Model/Entity/Article.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Collection\Collection;
 
 class Article extends Entity
 {
@@ -10,4 +11,19 @@ class Article extends Entity
         'id' => false,
         'slug' => false,
     ];
+
+    protected function _getTagString()
+    {
+        if (isset($this->_properties['tag_string'])) {
+            return $this->_properties['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->title . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
--- src/Template/Articles/edit.ctp
+++ src/Template/Articles/edit.ctp
@@ -4,7 +4,7 @@
     echo $this->Form->control('user_id', ['type' => 'hidden']);
     echo $this->Form->control('title');
     echo $this->Form->control('body', ['rows' => '3']);
-    echo $this->Form->control('tags._ids', ['options' => $tags]);
+    echo $this->Form->control('tags.tag_string', ['options' => $tags]);
     echo $this->Form->button(__('Save Article'));
     echo $this->Form->end();
 ?>
